4e7c30c9908a3e697960618caae3821a26185fed
Vladikkibich <vladikkibich@gmail.com>
2018-08-08 23:44:49 +0300

    first push

---
+++ app/js/common.js
@@ -0,0 +1,16 @@
+$(function() {
+
+	$(document).ready(function () {
+		$('.popup-youtube, .popup-youtube__naightvision,  .popup-youtube__smokemirrors').magnificPopup({
+			disableOn: 700,
+			type: 'iframe',
+			mainClass: 'mfp-fade',
+			removalDelay: 160,
+			preloader: false,
+
+			fixedContentPos: false
+		});
+	});
+
+
+});
